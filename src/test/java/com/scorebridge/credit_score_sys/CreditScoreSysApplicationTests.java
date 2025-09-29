8a6f0303e3405c81ed3bb819831ea5a9ba2c890f
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-30 01:52:32 +0300

    Add JUnit test class for main application

---
+++ src/test/java/com/scorebridge/credit_score_sys/CreditScoreSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorebridge.credit_score_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditScoreSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
