da3f71434d120bcd0961097110d7a999166e9a07
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-09-30 01:52:29 +0300

    Add main Spring Boot application entry point class

---
+++ src/main/java/com/scorebridge/credit_score_sys/CreditScoreSysApplication.java
@@ -0,0 +1,13 @@
+package com.scorebridge.credit_score_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoreSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditScoreSysApplication.class, args);
+	}
+
+}
