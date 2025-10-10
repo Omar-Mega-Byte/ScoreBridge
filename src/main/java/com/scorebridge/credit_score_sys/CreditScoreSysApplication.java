dd57e915d4a0044288d5d65cdfcb1caacb960bc5
Omar Elrfaay <omar.tolis2004@gmail.com>
2025-10-10 20:00:01 +0300

    Enable scheduling support in CreditScoreSysApplication

--- src/main/java/com/scorebridge/credit_score_sys/CreditScoreSysApplication.java
+++ src/main/java/com/scorebridge/credit_score_sys/CreditScoreSysApplication.java
@@ -2,8 +2,10 @@ package com.scorebridge.credit_score_sys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CreditScoreSysApplication {
 
 	public static void main(String[] args) {
